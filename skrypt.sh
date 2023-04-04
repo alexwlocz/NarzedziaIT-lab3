8b811f59e43da3c02d11dd25e653632533145acc
Aleksandra <aleksandrawloczysiak@gmail.com>
2023-04-04 22:35:14 +0200

    Loops for create x files added

--- skrypt.sh
+++ skrypt.sh
@@ -9,11 +9,22 @@ fi
 
 #Tworzenie automatycznie 100 plików
 
-if [ "$1" = "--logs" ]; then
+if [ "$1" = "--logs" ] && [ "$2" = "" ]; then
     for i in {1..100}; do
         mkdir log$i
         echo "Nazwa pliku: log$i.txt" > ./log$i/log$i.txt
         echo "Nazwa skryptu: skrypt.sh" >> ./log$i/log$i.txt
         echo "Data utworzenia: $(date)" >> ./log$i/log$i.txt
     done
+
+#Tworzenie i plików
+
+elif [ "$1" = "--logs" ] && [ "$2" -gt 0 ] 2>/dev/null; then
+    for ((i=1; i<=$2; i++)); do
+        mkdir log$i
+        echo "Nazwa pliku: log$i.txt" > ./log$i/log$i.txt
+        echo "Nazwa skryptu: skrypt.sh" >> ./log$i/log$i.txt
+        echo "Data utworzenia: $(date)" >> ./log$i/log$i.txt
+    done
 fi  
+
